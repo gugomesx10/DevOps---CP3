567de87563c894df11efd200ae6ec119d93d5c4d
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-22 15:23:19 -0300

    Editei o docker para criar as ACR corretamente

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 
 sequelize.sync().then(() => {
   app.listen(PORT);
-}).catch(() => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+}).catch((err) => {
+  console.error('Erro ao conectar ao banco de dados:', err.message);
   process.exit(1);
 });
